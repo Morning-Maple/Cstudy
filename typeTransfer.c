58acc4fc57afc19462ac7338d07c5d87a87ae030
Morning-Maple <1404126751@qq.com>
2022-08-29 00:03:49 +0800

    type

--- typeTransfer.c
+++ typeTransfer.c
@@ -41,4 +41,36 @@ void main(){
     f1 = d2;    //此处会出现精度损失（double -> float）
     printf("f1=%.6f", f1);  //期望：4.898989    ;实际：4.898990
 
+    printf("=====");
+    //强制转换（高精度转低精度）
+    double dd1 = 1.234;
+    int numb1 = dd1;
+    printf("\n%d", numb1);
+    //dd1转numb1报错，提醒会出现精度丢失
+    int numb2 = (int)dd1;
+    printf("\n%d", numb2);
+    //dd1转numb2正常，但是小数点后全部数据丢失（不存在四舍五入）
+    //此举转换不改变操作数（dd1）本身的类型或数值
+
+    //强制转换只对最近的数值生效，可用小括号提高优先级，[(类型)表达式]例如：
+    
+    int x = (int)3.5*10+7*8.4;  //理论是等于58.8+35=93.8；实际是58.8+30=88.8；注意：由于定义的x和y都是整型，所以小数点后不输出！！
+    int y = (int)(3.5*10+7*8.4);    //理论是93.8；实际是93；
+
+    //x中只改变10
+    //y则对后面括号内的数
+    printf("\n%d", x);
+    printf("\n%d", y);
+
+
+    char b = 'a';
+    int i = 5;
+    float ddd = .314;
+    double ddd2 = 1.0;
+    double res = b + i + ddd;   //res精度损失，警告float ->double
+    char ress = b + i + ddd + ddd2; //ress精度损失，警告float ->char
+    printf("\n%lf", res);
+    printf("\n%c", ress);
+
+
 }
